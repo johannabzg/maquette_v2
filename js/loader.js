68e9c8416f29eab957cceb809f7eea9eb112494b
johannabzg <johanna.bouzige@gmail.com>
2017-05-02 20:41:25 +0200

    update

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,20 @@
-$(document).ready(function () {
- 
- setTimeout("location.href = 'index.html';", 5000);
- 
+$(document).ready(function(){
+             
+     $('#char_2').css('opacity', 0);
+     $('#char_1').show;
+    
+    function charge_page(){
+     $('#char_2').css('opacity', 10);
+     $('#char_1').css('opacity', 0);
+};
+    setTimeout(charge_page, 4000);
+
 });
+
+/*
+ var apparition = function(){
+    $("#char_2").hide;
+    $("#char_1").show;
+}
+ setTimeout(apparition, 1000);
+*/
