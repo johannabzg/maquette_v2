0f3ff3b05b95e29bd41b4ea0cdba6153e0aaa980
johannabzg <johanna.bouzige@gmail.com>
2017-05-11 15:09:37 +0200

    modification du temp de chargement

--- js/loader.js
+++ js/loader.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
      $('#char_2').css('opacity', 10);
      $('#char_1').css('opacity', 0);
 };
-    setTimeout(charge_page, 2000);
+    setTimeout(charge_page, 4000);
 
 });
 
