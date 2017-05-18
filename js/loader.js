56a5c8df1fd0b0fb4dd898cb1fd802480828c9b9
johannabzg <johanna.bouzige@gmail.com>
2017-05-18 12:49:51 +0200

    today

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-             
+
      $('#char_2').css('opacity', 0);
      $('#char_1').show;
-    
+
     function charge_page(){
      $('#char_2').css('opacity', 10);
      $('#char_1').css('opacity', 0);
